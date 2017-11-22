577c6928c89ce6724b045ab1be3398297bd270cb
zibinmei <zibinmei@outlook.com>
2017-11-22 15:15:20 -0700

    clean up

--- SourceCode/src/Eval.java
+++ SourceCode/src/Eval.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
 
 //this code is trying to optimize for speed
 public class Eval {
