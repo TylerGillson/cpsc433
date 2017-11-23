3c559b3eead87db0ae666c90e2f9f3bd638e4de4
Bigfootmandude <davidjkeizer@gmail.com>
2017-11-23 09:48:57 -0700

    Updated to use lists natively

--- SourceCode/src/Selection.java
+++ SourceCode/src/Selection.java
@@ -7,41 +7,42 @@ import java.util.List;
 public class Selection
 {
 	public static boolean debug = false;
-	private int[][] factArray;
+	private List<int[]> generation;
 	private float[] evalArray;
 	private float[] indexArray;
 	private Random rand;
 	private int lastChoice;
 
 
-	//*****TODO********** I (David) need to convert this to use the list natively
+
 
 	/**
 	* Constructor takes in a population (the array of facts), and a Random object.
 	* @param int[][] facts, Random rand, where facts is the population and random is an existing Random instance.
 	*/
-	public Selection(List<int[]> facts, Random rand)
+	public Selection(List<int[]> generation, Random rand)
 	{
 		this.rand = rand;
-		factArray = facts.toArray(new int[facts.size()][facts.get(0).length]);
-		evalArray = new float[factArray.length];
-		indexArray = new float[factArray.length];
+		int generationSize = generation.size();
+		this.generation = generation;// = facts.toArray(new int[facts.size()][facts.get(0).length]);
+		evalArray = new float[generationSize];
+		indexArray = new float[generationSize];
 
 		//Set up the array contents
-		for (int i = 0; i<factArray.length; i++)
+		for (int i = 0; i<generationSize; i++)
 		{
 			indexArray[i] = i;
 		}
 		int sum = 0;
 		int tempValue;
-		for (int i = 0; i< factArray.length; i++)
+		for (int i = 0; i< generationSize; i++)
 		{
-			tempValue = Driver.eval.getValue(factArray[i]);
+			tempValue = Driver.eval.getValue(generation.get(i));
 			evalArray[i] = tempValue;
 			sum += tempValue;
 		}
 		//Next we need to normalize each of the eval weights of the facts
-		for (int i = 0; i< factArray.length; i++)
+		for (int i = 0; i< generationSize; i++)
 		{
 			evalArray[i] = evalArray[i] /sum;
 		}
@@ -56,15 +57,15 @@ public class Selection
 	public int getLastIndexChoice()
 	{
 		try{
-			lastChoice +=-0;
+			lastChoice +=0;
 		}catch(Exception e)
 		{
 			throw new IllegalStateException();
 		}
 
 		return lastChoice;
-
 	}
+	
 	/**
 	* Method performs f_select based on the population that the object was instantiated with
 	* @param int ignoreFactIndex, the fact that is to not be used.
@@ -80,13 +81,13 @@ public class Selection
 				if (evalArray[Math.round(indexArray[i])] >= randFloat)
 				{
 					lastChoice = Math.round(indexArray[i]);
-					return factArray[Math.round(indexArray[i])];
+					return generation.get(Math.round(indexArray[i]));
 				}
 			}
 		}
 
 		lastChoice = Math.round(indexArray[0]);
-		return factArray[Math.round(indexArray[0])];
+		return generation.get(Math.round(indexArray[0]));
 	}
 
 
@@ -104,77 +105,12 @@ public class Selection
 			if (evalArray[Math.round(indexArray[i])] >= randFloat)
 			{
 				lastChoice = Math.round(indexArray[i]);
-				return factArray[Math.round(indexArray[i])];
+				return generation.get(Math.round(indexArray[i]));
 			}
 
 		}
 		lastChoice = Math.round(indexArray[0]);
-		return factArray[Math.round(indexArray[0])];
-	}
-
-
-
-
-	/**
-	* This method performs f_select without an object being instantiated.
-	* This should only be used if this will be called once with a set of facts. Otherwise an object should be instantiated
-	* @param int[][] facts, a 2d array, where the first array is the array of assignments
-	* @return int[] selectedFact, the fact chosen from the population via roulette selection
-	*/
-	public static int[] select_singleuse(int [][] facts)
-	{
-		//First we find the total eval value of each of the facts
-		int sum = 0;
-		if (facts.length == 0) throw new IllegalStateException();
-		float[] factEvals = new float[facts.length];
-		float[] courseIndexArray = new float[facts[0].length];
-
-		for (int i = 0; i<facts[0].length; i++)
-		{
-			courseIndexArray[i] = i;
-		}
-		//To prevent multiple array lookups we use tempValue to store the eval value
-		int tempValue;
-		for (int i = 0; i< facts.length; i++)
-		{
-			tempValue = tempEval(facts[i]);
-			factEvals[i] = tempValue;
-			sum += tempValue;
-		}
-		//Next we need to normalize each of the eval weights of the facts
-		for (int i = 0; i< facts.length; i++)
-		{
-			factEvals[i] = factEvals[i] /sum;
-		}
-
-
-		QuickSortWithIndex sorter = new QuickSortWithIndex(factEvals);
-		//We now need to sort the now normalized factEvals
-		float[][] temp = sorter.sort();
-		factEvals = temp[0];
-		courseIndexArray=  temp[1];
-
-		if(debug){
-			System.out.println("The normalized eval values is as follows: ");
-			for (int i = 0; i <factEvals.length; i++)
-			{
-				System.out.println(factEvals[Math.round(courseIndexArray[i])] + " at index " + Math.round(courseIndexArray[i]));
-			}
-		}
-
-		Random rand = new Random();
-		float randFloat = rand.nextFloat();  //We now have our random index.
-		for (int i = 0; i < factEvals.length; i++)
-		{
-			if (factEvals[Math.round(courseIndexArray[i])] >= randFloat)
-			{
-				return facts[Math.round(courseIndexArray[i])];
-			}
-
-		}
-
-		return facts[Math.round(courseIndexArray[0])];
-
+		return generation.get(Math.round(indexArray[0]));
 	}
 
 	public static int[][] sortFromEvals(int[][] facts)
