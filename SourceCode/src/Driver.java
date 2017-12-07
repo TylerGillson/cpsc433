d235730e924be2f289b0898366c542f33b5c06a1
Tyler Gillson <tyler.gillson@gmail.com>
2017-12-07 12:00:51 -0700

    Catching stack overflow errors.

--- SourceCode/src/Driver.java
+++ SourceCode/src/Driver.java
@@ -215,7 +215,11 @@ public class Driver {
         	// Perform an or-tree-based search to build a solution candidate:
         	ArrayList<Integer> mostTightlyBound = getMTBCopy();
         	Random rand = new Random();
-        	candidate = t.buildCandidate(mostTightlyBound, 0, leafHeap, rand);
+        	try { candidate = t.buildCandidate(mostTightlyBound, 0, leafHeap, rand); }
+        	catch (StackOverflowError e) {
+        		System.out.println("Please re-execute with more stack memory allocated. Use: -Xss[a larger number]m.");
+        		System.exit(0);
+        	}
         	
            	if (candidate == null) {
 				System.out.println("No solution exists.");
--- SourceCode/src/Generation.java
+++ SourceCode/src/Generation.java
@@ -27,7 +27,11 @@ public class Generation{
     	// Perform an or-tree-based search to build a solution candidate:
     	ArrayList<Integer> mostTightlyBound = Driver.getMTBCopy();
     	Random rand = new Random();
-    	candidate = t.buildCandidate(mostTightlyBound, 0, leafHeap, rand);
+    	try{ candidate = t.buildCandidate(mostTightlyBound, 0, leafHeap, rand); }
+    	catch (StackOverflowError e) {
+    		System.out.println("Please re-execute with more stack memory allocated. Use: -Xss[a larger number]m.");
+    		System.exit(0);
+    	}
     	this.generation.add(candidate);
 	}
 	
